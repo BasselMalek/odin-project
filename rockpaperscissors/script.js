8c7a6e6d0ed5f6a558caadfac141f49267540980
Bassel W. Ibrahim <bassel.walid04@gmail.com>
2023-06-13 21:08:01 +0300

    feat(DegradationRockPaperScissors): full session.

--- rockpaperscissors/script.js
+++ rockpaperscissors/script.js
@@ -18,29 +18,70 @@ function getComputerChoice() {
     }
 }
 
-function excuteRound(player_choice, computer_choice) {
-    player_choice = player_choice.toLowerCase(player_choice);
+function excuteRound() {
+    let player_choice = prompt(
+        "Enter your choice for this round",
+        "paper"
+    ).toLowerCase();
+    let computer_choice = getComputerChoice();
     if (player_choice == computer_choice) {
-        return "Draw! You're both dumb!";
+        alert("Draw! You're both dumb!");
     } else if (player_choice == "rock") {
         if (computer_choice == "paper") {
-            return `A computer just beat your ass! It picked ${computer_choice}`;
+            alert(
+                `A computer just beat your ass! It picked ${computer_choice}`
+            );
+            return 0;
         } else {
-            return `You a beat computer's ass! It picked ${computer_choice}`;
+            alert(`You a beat computer's ass! It picked ${computer_choice}`);
+            return 1;
         }
     } else if (player_choice == "paper") {
         if (computer_choice == "scissors") {
-            return `A computer just beat your ass! It picked ${computer_choice}`;
+            alert(
+                `A computer just beat your ass! It picked ${computer_choice}`
+            );
+            return 0;
         } else {
-            return `You a beat computer's ass! It picked ${computer_choice}`;
+            alert(`You a beat computer's ass! It picked ${computer_choice}`);
+            return 1;
         }
     } else if (player_choice == "scissors") {
         if (computer_choice == "rock") {
-            return `A computer just beat your ass! It picked ${computer_choice}`;
+            alert(
+                `A computer just beat your ass! It picked ${computer_choice}`
+            );
+            return 0;
         } else {
-            return `You a beat computer's ass! It picked ${computer_choice}`;
+            alert(`You a beat computer's ass! It picked ${computer_choice}`);
+            return 1;
         }
     } else {
-        return `wtf does ${player_choice} even mean? you understand the game right?`;
+        alert(
+            `wtf does ${player_choice} even mean? you understand the game right?`
+        );
+        return 0;
+    }
+}
+function startGameSession(rounds = 5) {
+    let player_score = 0,
+        computer_score = 0;
+    for (let i = 0; i < rounds; i++) {
+        let result = excuteRound();
+        if (result) {
+            player_score++;
+        } else {
+            computer_score++;
+        }
+    }
+    if (player_score == computer_score) {
+        excuteRound();
+    }
+    if (player_score > computer_score) {
+        alert("You won. Woo.");
+    } else {
+        alert(
+            `The computer curb-stompped you ${computer_score} times. Severe skill issue on your side.`
+        );
     }
 }
