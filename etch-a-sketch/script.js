00b0d774b3ee73fdfb89302a2462bdcfa245d39d
Bassel W. Ibrahim <bassel.walid04@gmail.com>
2023-06-29 19:50:57 +0300

    project(smiths-etch): alpha version

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,11 +1,32 @@
+function smithsPaint(object) {
+    let decsision = Math.floor(Math.random() * 4);
+    console.log(decsision);
+    switch (decsision) {
+        case 0:
+            object.classList.toggle("smiths");
+            break;
+        case 1:
+            object.classList.toggle("queen");
+        case 2:
+            object.classList.toggle("strangeways");
+        case 3:
+            object.classList.toggle("meat");
+        default:
+            break;
+    }
+}
+
 function createGrid(horizontal, vertical) {
-    const containerDiv = document.getElementById("etch-container");
+    const containerDiv = document.getElementById("etch-pad");
     for (let i = 0; i < horizontal; i++) {
         const horDiv = document.createElement("div");
         horDiv.classList.toggle("horizontal-grid");
         for (let k = 0; k < vertical; k++) {
             const verDiv = document.createElement("div");
             verDiv.classList.toggle("vertical-grid");
+            verDiv.addEventListener("click", () => {
+                smithsPaint(verDiv);
+            });
             horDiv.appendChild(verDiv);
         }
         containerDiv.appendChild(horDiv);
