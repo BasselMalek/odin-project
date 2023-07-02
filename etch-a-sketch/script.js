544ac493e611047811a273ffd14f8fbd922f05e7
Bassel W. Ibrahim <bassel.walid04@gmail.com>
2023-07-03 00:02:51 +0300

    proj(smiths-etch): full.

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,4 +1,18 @@
+let PAINT_MODE = "smiths";
+
+function genRanHex() {
+    return [...Array(6)]
+        .map(() => Math.floor(Math.random() * 16).toString(16))
+        .join("");
+}
+function rainbowPaint(object) {
+    let color = genRanHex();
+    object.style.backgroundColor = `#${color}`;
+}
+
 function smithsPaint(object) {
+    object.className = "";
+    object.classList.toggle("vertical-grid");
     let decsision = Math.floor(Math.random() * 4);
     console.log(decsision);
     switch (decsision) {
@@ -7,10 +21,13 @@ function smithsPaint(object) {
             break;
         case 1:
             object.classList.toggle("queen");
+            break;
         case 2:
             object.classList.toggle("strangeways");
+            break;
         case 3:
             object.classList.toggle("meat");
+            break;
         default:
             break;
     }
@@ -25,7 +42,11 @@ function createGrid(horizontal, vertical) {
             const verDiv = document.createElement("div");
             verDiv.classList.toggle("vertical-grid");
             verDiv.addEventListener("click", () => {
-                smithsPaint(verDiv);
+                if (PAINT_MODE === "smiths") {
+                    smithsPaint(verDiv);
+                } else {
+                    rainbowPaint(verDiv);
+                }
             });
             horDiv.appendChild(verDiv);
         }
@@ -33,11 +54,33 @@ function createGrid(horizontal, vertical) {
     }
 }
 
+function eraseGrid() {
+    const containerDiv = document.getElementById("etch-pad");
+    containerDiv.replaceChildren();
+}
+
 window.onload = function () {
+    const modeID = document.getElementById("mode");
     const createBtn = document.getElementById("create");
     createBtn.addEventListener("click", () => {
         let gridHz = prompt("Enter horizontal"),
             gridVr = prompt("Enter vertical");
         createGrid(gridHz, gridVr);
+    });
+    const eraseBtn = document.getElementById("erase");
+    eraseBtn.addEventListener("click", () => {
+        eraseGrid();
+    });
+    const rainbowBtn = document.getElementById("rainbow-toggle");
+    rainbowBtn.addEventListener("click", () => {
+        eraseGrid();
+        PAINT_MODE = "rainbow";
+        modeID.textContent = PAINT_MODE;
+    });
+    const smithsBtn = document.getElementById("smiths-toggle");
+    smithsBtn.addEventListener("click", () => {
+        eraseGrid();
+        PAINT_MODE = "smiths";
+        modeID.textContent = PAINT_MODE;
     });
 };
